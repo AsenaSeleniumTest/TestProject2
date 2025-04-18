8af6b612496fc834b721f933815dd321ebb33d74
Lord USBQ <buendiau@gmail.com>
2025-04-18 08:05:32 -0600

    Test 2 running tests git actions April 18

--- unittest/test_filemanager.py
+++ unittest/test_filemanager.py
@@ -2,8 +2,8 @@ import os
 import pytest
 from Utility.file_manager import File_Manager
 
-@pytest.mark.filemanager
-def test_readfile():
+#@pytest.mark.filemanager
+def s_test_readfile():
     fm=File_Manager
     data = fm.read_file(fm,filename="./Utility/network1.txt")
     for d in data:
